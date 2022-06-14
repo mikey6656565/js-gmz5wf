dc684af7f4d5386040134577003208b769f77965
mikey6656565 <juniormanz2007@gmail.com>
2022-06-14 12:12:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-gmz5wf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-gmz5wf)
---
+++ index.js
@@ -0,0 +1,15 @@
+//define age
+let age = 18;
+let age2 = 14;
+
+//If age meets requirments, user may sign up, if not, deny
+if (age >= 18) {
+  console.log('you can sign up');
+} else {
+  console.log('you must be 18 to sign up');
+}
+if (age2 >= 14) {
+  console.log('You may sign up with limited permissions');
+} else {
+  console.log('you just be 14 or older to sign up');
+}
